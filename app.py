937476e59f7f4ce94ccd519ed4685bf29b7e2284
Aditk23 <adit.k2398@gmail.com>
2021-09-18 18:34:27 +0530

    Deployment

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
 # Import libraries
-from types import MethodDescriptorType
+#from types import MethodDescriptorType
 from flask import Flask,render_template,request
 import joblib
 
 # create instance of an app
 app = Flask(__name__)
 tfidf = joblib.load('tfidf_vector_model.pkl')
-model = joblib.load('netflix_baseline.pkl')
+model = joblib.load('netflix_model.pkl')
 @app.route('/')
 def welcome():
     return render_template('index.html')
@@ -14,13 +14,15 @@ def welcome():
 @app.route('/input',methods=['POST'])
 def inputs():
     data = request.form.get('review')
-    vector = tfidf.transform(data).toarray()
+    lst = []
+    lst.append(data)
+    vector = tfidf.transform(lst).toarray()
     prediction = model.predict(vector)
     if(prediction[0]==1):
         output = 'Positive Review'
     else:
         output='Negative Review'
-    return render_template('output.html',predicted_text = {output})
+    return render_template('output.html',predicted_text = output)
 
 @app.route('/again',methods=['POST'])
 def again():
